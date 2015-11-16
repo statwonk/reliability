b15db77efbaa1da2ccf976faeb66a91296f01cfd
statwonk <cpeter9@gmail.com>
2015-11-16 10:42:52 -0600

    Test new syntax in a toy shiny app

--- shiny.R
+++ shiny.R
@@ -4,16 +4,17 @@ library(magrittr)
 library(shinyAce)
 
 ui = shinyUI(fluidPage(
-  aceEditor('code', mode='r', value="create_graph() %>%
-  add_node_df(create_nodes(c('Internet', 'Load Balancer', 'Web Server'))) %>%
-            add_edge_df(create_edges(from = c('Internet', 'Load Balancer'),
-            to = c('Load Balancer', 'Web Server'))) %>%
-            set_node_attr('Internet', 'shape', 'plaintext') %>%
-            set_node_attr('Load Balancer', 'reliability', 0.8) %>%
-            set_node_attr('Web Server', 'reliability', 0.9) %>%
-            render_graph()"),
-  actionButton("eval", "Evaluate"),
-  grVizOutput('diagram')
+  fluidRow(
+    column(4,
+           aceEditor('code', mode='r', value="create_system() %>%
+  node('load balancer', 0.9) %>%
+  node('database', 0.9) %>%
+  node('web server', 0.9) %>%
+  plot()"),
+           actionButton("eval", "Evaluate")
+    ),
+    column(8, grVizOutput('diagram'))
+  )
 ))
 
 server = shinyServer(function(input, output) {
